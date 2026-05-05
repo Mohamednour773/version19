0fe33db41a37ecf5f1f944b8947181e29b35e910
Mohamednour426 <mohamed.nour426@icloud.com>
2026-05-05 09:53:44 +0300

    Approval Cycle

--- universal_approval_cycle/models/purchase_order.py
+++ universal_approval_cycle/models/purchase_order.py
@@ -21,6 +21,23 @@ class PurchaseOrder(models.Model):
                 and order.approval_state != 'approved'
             )
 
+    def _check_purchase_state_approval_lock(self, target_state):
+        """Block purchase confirmation state transitions until approval completes."""
+        blocked_states = {'to approve', 'purchase', 'done'}
+        if target_state not in blocked_states:
+            return
+        for order in self:
+            order._check_approval_action_allowed(
+                action_name='button_confirm',
+                action_label=_('confirm this purchase order'),
+            )
+
+    def write(self, vals):
+        target_state = vals.get('state')
+        if target_state:
+            self._check_purchase_state_approval_lock(target_state)
+        return super().write(vals)
+
     def button_confirm(self):
         for order in self:
             order._check_approval_action_allowed(
