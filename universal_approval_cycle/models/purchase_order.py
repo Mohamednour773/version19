14c735d7cd03317ed1be38d896a0f7ae07996aaf
Mohamednour426 <mohamed.nour426@icloud.com>
2026-05-05 09:48:20 +0300

    Approval Cycle

--- universal_approval_cycle/models/purchase_order.py
+++ universal_approval_cycle/models/purchase_order.py
@@ -3,11 +3,23 @@
 Purchase Order integration for the universal approval engine.
 """
 
-from odoo import models, _
+from odoo import fields, models, _
 
 
 class PurchaseOrder(models.Model):
     _inherit = ['purchase.order', 'univ.approval.mixin']
+
+    can_confirm_with_approval = fields.Boolean(
+        string='Can Confirm With Approval',
+        compute='_compute_can_confirm_with_approval',
+    )
+
+    def _compute_can_confirm_with_approval(self):
+        for order in self:
+            order.can_confirm_with_approval = not (
+                order._approval_requires_action_block('button_confirm')
+                and order.approval_state != 'approved'
+            )
 
     def button_confirm(self):
         for order in self:
