cc7e4cda9606ba9d12c88504357731bfddcea6b9
Mohamednour426 <mohamed.nour426@icloud.com>
2026-05-09 04:54:09 +0300

    Approval

--- files/apw_workflow/models/apw_config.py
+++ files/apw_workflow/models/apw_config.py
@@ -16,7 +16,7 @@ class ApwConfig(models.Model):
 
     model_id = fields.Many2one(
         'ir.model', string='Target Model', required=True,
-        ondelete='restrict', domain=[('transient', '=', False)],
+        ondelete='set null', domain=[('transient', '=', False)],
     )
     model_name = fields.Char(
         related='model_id.model', string='Model Technical Name',
