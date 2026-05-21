6bc76a9ee53e0a591ea7d846d7f9d4a77396f5b1
Mohamednour426 <mohamed.nour426@icloud.com>
2026-05-21 19:04:21 +0300

    Ionic

--- addons/club_management_pos/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import club_package
-from . import pos_config
-from . import pos_order
-from . import product_template
--- addons/club_management_system_final/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import generate_sessions_wizard
-from . import reschedule_session_wizard
